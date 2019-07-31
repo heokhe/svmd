b0da0484e8d43b2fc6c04844f3532a7b61d028e9
Hosein Khansari <hrk.x11@gmail.com>
2019-08-01 00:25:03 +0430

    make big changes in TopAppBar
    use component action.
    add props: title, dense, prominent, short, etc.
    use IconButton for nav icon.
    add `noNavIcon` to optionally remove nav icon.
    fire 'nav-icon-click' when nav button is clicked.
    add a slot tag.

--- src/TopAppBar.svelte
+++ src/TopAppBar.svelte
@@ -1,23 +1,41 @@
 <script>
 import '@material/top-app-bar/mdc-top-app-bar.scss';
+import IconButton from './IconButton.svelte';
+import Icon from './Icon.svelte';
 import { MDCTopAppBar } from '@material/top-app-bar';
-import { onMount, onDestroy } from 'svelte';
+import { component } from './actions';
+import { createEventDispatcher } from 'svelte';
 
-export let fixed = false;
-let root, mdc;
-onMount(() => {
-  mdc = new MDCTopAppBar(root);
-})
-onDestroy(() => {
-  mdc.destroy()
-})
+export let fixed = false,
+  noNavIcon = false,
+  dense = false,
+  prominent = false,
+  short = false,
+  alwaysClosed = false,
+  title = '';
+
+const dispatch = createEventDispatcher()
 </script>
 
-<header class="mdc-top-app-bar" bind:this={root} class:mdc-top-app-bar--fixed={fixed}>
+<header
+  class="mdc-top-app-bar"
+  use:component={MDCTopAppBar}
+  class:mdc-top-app-bar--fixed={fixed}
+  class:mdc-top-app-bar--dense={dense}
+  class:mdc-top-app-bar--prominent={prominent}
+  class:mdc-top-app-bar--short={short}
+  class:mdc-top-app-bar--short-collapsed={alwaysClosed}>
   <div class="mdc-top-app-bar__row">
     <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-      <button class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button">menu</button>
-      <span class="mdc-top-app-bar__title">Title</span>
+      {#if !noNavIcon}
+        <IconButton class="mdc-top-app-bar__navigation-icon" disabled on:click={() => dispatch('nav-icon-click')}>
+          <Icon>menu</Icon>
+        </IconButton>
+      {/if}
+      <span class="mdc-top-app-bar__title">{title}</span>
+    </section>
+    <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+      <slot></slot>
     </section>
   </div>
 </header>
