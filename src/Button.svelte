6806dd18dfbe3fad99f927948fd13a872f016877
Hosein Khansari <hrk.x11@gmail.com>
2019-08-01 00:18:22 +0430

    (button) forward unknown props to the root element, add some event listeners

--- src/Button.svelte
+++ src/Button.svelte
@@ -1,19 +1,22 @@
 <script>
 import '@material/button/mdc-button.scss';
 import { rippleAction as ripple } from './actions';
+import { omit } from './helpers';
 
 export let dense = false,
   raised = false,
   outlined = false,
   unelevated = false;
+$: props = omit($$props, 'dense', 'raised', 'outlined', 'unelevated');
 </script>
 
-<button class="mdc-button"
-  use:ripple
+<button {...props} use:ripple
+  class="mdc-button"
   class:mdc-button--dense={dense}
   class:mdc-button--outlined={outlined}
   class:mdc-button--unelevated={unelevated}
-  class:mdc-button--raised={raised}>
+  class:mdc-button--raised={raised}
+  on:click on:keydown on:keypress on:keyup on:mouseover>
   <span class="mdc-button__label">
     <slot></slot>
   </span>
