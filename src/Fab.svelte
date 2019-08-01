7aa95ac581e47206a3862d141a0fbe43908f81ab
Hosein Khansari <hrk.x11@gmail.com>
2019-08-01 17:09:33 +0430

    add Fab component

---
+++ src/Fab.svelte
@@ -0,0 +1,41 @@
+<script>
+import '@material/fab/mdc-fab.scss';
+import { ripple } from './actions';
+import { createClassname } from './helpers';
+import Icon from './Icon.svelte';
+export let icon = '',
+  extended = false,
+  exited = false,
+  fixed = false,
+  iconTrailing = false,
+  mini = false;
+$: className = createClassname('mdc-fab', { extended, mini, exited, fixed })
+</script>
+
+<button use:ripple class={className} on:click>
+  {#if icon && !iconTrailing}
+    <div class="mdc-fab__icon"><Icon>{icon}</Icon></div>
+  {/if}
+  {#if extended}
+    <span class="mdc-fab__label">
+      <slot></slot>
+    </span>
+  {/if}
+  {#if icon && iconTrailing}
+    <div class="mdc-fab__icon"><Icon>{icon}</Icon></div>
+  {/if}
+</button>
+
+<style>
+:global(.mdc-fab.mdc-fab--fixed) {
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+}
+@media only screen and (min-width: 1024px) {
+  :global(.mdc-fab.mdc-fab--fixed) {
+    bottom: 1.5rem;
+    right: 1.5rem;
+  }
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as TopAppBar } from './TopAppBar.svelte';
 export { default as Icon } from './Icon.svelte';
 export { default as IconButton } from './IconButton.svelte';
 export { default as Drawer } from './Drawer.svelte';
+export { default as Fab } from './Fab.svelte';
