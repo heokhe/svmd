bff35402619306d424f0ffe4130a0d790101e2b9
Hosein Khansari <hrk.x11@gmail.com>
2019-08-01 11:35:28 +0430

    (icon button) fix indentation

--- src/IconButton.svelte
+++ src/IconButton.svelte
@@ -4,5 +4,5 @@ import { rippleAction as ripple } from './actions';
 </script>
 
 <button class="mdc-icon-button" use:ripple={{ unbounded: true }} on:click>
-   <slot class="mdc-icon-button__icon"></slot>
+  <slot class="mdc-icon-button__icon"></slot>
 </button>
