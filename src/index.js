8a443b9d610debf57e29c00926e415dc6fa4afc4
Hosein Khansari <hrk.x11@gmail.com>
2019-08-01 11:36:30 +0430

    export Icon & IconButton, save @material/icon-button in dependencies

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import Button from './Button.svelte';
 import TopAppBar from './TopAppBar.svelte';
+import Icon from './Icon.svelte';
+import IconButton from './IconButton.svelte';
 
-export { Button, TopAppBar };
+export {
+  Button, TopAppBar, Icon, IconButton
+};
