913eb0cb0061a49af0ee5fdaa762159130a22e44
Hosein Khansari <hrk.x11@gmail.com>
2019-08-01 17:01:28 +0430

    (button) fix props reset when component is re-rendered

--- src/Button.svelte
+++ src/Button.svelte
@@ -1,22 +1,17 @@
 <script>
 import '@material/button/mdc-button.scss';
-import { rippleAction as ripple } from './actions';
-import { omit } from './helpers';
+import { ripple } from './actions';
+import { omit, createClassname } from './helpers';
 
 export let dense = false,
   raised = false,
   outlined = false,
   unelevated = false;
 $: props = omit($$props, 'dense', 'raised', 'outlined', 'unelevated');
+$: className = createClassname('mdc-button', { dense, outlined, unelevated, raised })
 </script>
 
-<button {...props} use:ripple
-  class="mdc-button"
-  class:mdc-button--dense={dense}
-  class:mdc-button--outlined={outlined}
-  class:mdc-button--unelevated={unelevated}
-  class:mdc-button--raised={raised}
-  on:click on:keydown on:keypress on:keyup on:mouseover>
+<button {...props} use:ripple class={className} on:click on:keydown on:keypress on:keyup on:mouseover>
   <span class="mdc-button__label">
     <slot></slot>
   </span>
