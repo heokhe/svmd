3d6bced5199cc5e2476c049ad7bce5a07250c2b1
Hosein Khansari <hrk.x11@gmail.com>
2019-08-05 13:34:06 +0430

    convert modifiers' name to kebab-case in createClassname()

--- src/helpers.js
+++ src/helpers.js
@@ -5,16 +5,18 @@ export const omit = (obj, ...keys) => Object.keys(obj)
   }))
   .reduce((a, b) => ({ ...a, ...b }), {});
 
+const camelToKebab = str => str.replace(/([A-Z])/g, $1 => `-${$1.toLowerCase()}`);
+
 /**
  * @example
- * createClassname('button', { raised: true, unelevated: true, outlined: false });
- * // 'mdc-button mdc-button--raised mdc-button--unelevated'
+ * createClassname('button', { raised: true, displayMarkers: true, outlined: false });
+ * // 'mdc-button mdc-button--raised mdc-button--display-markers'
  */
 export const createClassname = (base, modifiers = {}) => [
   `mdc-${base}`,
   ...Object.entries(modifiers)
     .filter(([, v]) => !!v)
-    .map(([k]) => `mdc-${base}--${k}`)
+    .map(([k]) => `mdc-${base}--${camelToKebab(k)}`)
 ].join(' ');
 
 /**
