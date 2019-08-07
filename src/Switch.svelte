4a7dd8e5317131ca6b0428709a27e7fd2b706c21
Hosein Khansari <hrk.x11@gmail.com>
2019-08-07 13:24:41 +0430

    add switch component

---
+++ src/Switch.svelte
@@ -0,0 +1,29 @@
+<script>
+  import '@material/switch/mdc-switch.scss';
+  import { wrap } from './actions';
+  import { createClassname } from './helpers';
+  import { MDCSwitch } from '@material/switch';
+
+  export let checked = false;
+  const mdc = wrap(MDCSwitch, {
+    initialize(_, mdcSwitch) {
+      mdcSwitch.checked = checked;
+      mdcSwitch.listen('change', () => {
+        checked = mdcSwitch.checked;
+      })
+    },
+    update(_, mdcSwitch, checked) {
+      mdcSwitch.checked = checked;
+    }
+  })
+</script>
+
+<div class="mdc-switch" use:mdc={checked}>
+  <div class="mdc-switch__track"></div>
+  <div class="mdc-switch__thumb-underlay">
+    <div class="mdc-switch__thumb">
+      <input type="checkbox" class="mdc-switch__native-control" role="switch">
+    </div>
+  </div>
+</div>
+<!-- <label for="basic-switch">off/on</label> -->
