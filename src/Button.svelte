3d84db094a74c60034e6ec28829627a19a78fb4d
Hosein Khansari <hrk.x11@gmail.com>
2019-08-16 14:59:30 +0430

    (button) add href prop

--- src/Button.svelte
+++ src/Button.svelte
@@ -5,13 +5,22 @@ import { omit, createClassname } from './helpers';
 export let dense = false,
   raised = false,
   outlined = false,
-  unelevated = false;
-$: props = omit($$props, 'dense', 'raised', 'outlined', 'unelevated');
+  unelevated = false,
+  href = '';
+$: props = omit($$props, 'dense', 'raised', 'outlined', 'unelevated', 'href');
 $: className = createClassname('button', { dense, outlined, unelevated, raised })
 </script>
 
-<button {...props} use:ripple class={className} on:click on:keydown on:keypress on:keyup on:mouseover>
-  <span class="mdc-button__label">
-    <slot></slot>
-  </span>
-</button>
+{#if href}
+  <a {href} {...props} use:ripple class={className} on:click on:keydown on:keypress on:keyup on:mouseover>
+    <span class="mdc-button__label">
+      <slot></slot>
+    </span>
+  </a>
+{:else}
+  <button {...props} use:ripple class={className} on:click on:keydown on:keypress on:keyup on:mouseover>
+    <span class="mdc-button__label">
+      <slot></slot>
+    </span>
+  </button>
+{/if}
