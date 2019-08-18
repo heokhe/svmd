886af5d2538b0ec2f52abc889326c27ad656b365
Hosein Khansari <hrk.x11@gmail.com>
2019-08-18 19:43:21 +0430

    add icon prop to Button

--- src/Button.svelte
+++ src/Button.svelte
@@ -1,4 +1,5 @@
 <script>
+import Icon from './Icon.svelte';
 import { ripple } from './actions';
 import { omit, createClassname } from './helpers';
 
@@ -6,6 +7,7 @@ export let dense = false,
   raised = false,
   outlined = false,
   unelevated = false,
+  icon = '',
   href = '';
 
 $: props = omit($$props, 'dense', 'raised', 'outlined', 'unelevated', 'href');
@@ -14,12 +16,18 @@ $: className = createClassname('button', { dense, outlined, unelevated, raised }
 
 {#if href}
   <a {href} {...props} use:ripple class={className} on:click on:keydown on:keypress on:keyup on:mouseover>
+    {#if icon}
+      <span class="mdc-button__icon material-icons">{icon}</span>
+    {/if}
     <span class="mdc-button__label">
       <slot></slot>
     </span>
   </a>
 {:else}
   <button {...props} use:ripple class={className} on:click on:keydown on:keypress on:keyup on:mouseover>
+    {#if icon}
+      <span class="mdc-button__icon material-icons">{icon}</span>
+    {/if}
     <span class="mdc-button__label">
       <slot></slot>
     </span>
