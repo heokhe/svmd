6782556f0f8239352e34fbebb09806a55ebdafa3
Hosein Khansari <hrk.x11@gmail.com>
2019-08-19 19:55:57 +0430

    (TextField) add outlined, leadingIcon and trailingIcon props

--- src/TextField.svelte
+++ src/TextField.svelte
@@ -1,46 +1,67 @@
 <script>
   import { wrap } from './actions';
-  import { createClassname } from './helpers';
+  import { cls, subcls } from './helpers';
   import { MDCTextField } from '@material/textfield';
 
   export let type = 'text',
     value = '',
     fullwidth = false,
-    // outlined = false,
+    outlined = false,
     label = '',
+    leadingIcon = '',
+    trailingIcon = '',
     placeholder = '';
 
   const mdc = wrap(MDCTextField, {
-    initialize({ root_: input }) {
+    initialize(mdc) {
+      console.log(mdc);
+      const input = mdc.root_;
       input.addEventListener('input', ({ target }) => {
         value = target.value;
       })
     }
   });
 
-  $: className = createClassname('text-field', {
+  $: c = cls('text-field', {
     fullwidth,
-    // outlined,
-    noLabel: !fullwidth && !label
-  })
+    outlined,
+    noLabel: !fullwidth && !label,
+    withLeadingIcon: !!leadingIcon,
+    withTrailingIcon: !!trailingIcon
+  });
+  $: outlineC = cls`notched-outline`;
+  $: floatingLabelC = cls`floating-label`;
 </script>
 
-<div class={className} use:mdc>
+<div class={c} use:mdc>
+  {#if leadingIcon}
+    <span class="material-icons {subcls(c, 'icon')}">
+      {leadingIcon}
+    </span>
+  {/if}
   <input {type} {value} {placeholder}
-    class="mdc-text-field__input"
+    class={subcls(c, 'input')}
     aria-label={label}
     on:input on:focus on:blur on:change
     on:keydown on:keyup on:keypress>
-  <!-- {#if outlined}
-    <div class="mdc-notched-outline">
-      <div class="mdc-notched-outline__leading"></div>
-      <div class="mdc-notched-outline__trailing"></div>
-    </div>
-  {/if} -->
-  {#if label}
-    <label class="mdc-floating-label">{label}</label>
+  {#if trailingIcon}
+    <span class="material-icons {subcls(c, 'icon')}">
+      {trailingIcon}
+    </span>
   {/if}
-  <!-- {#if !outlined} -->
+  {#if outlined}
+    <div class={outlineC}>
+      <div class={subcls(outlineC, 'leading')}></div>
+      <div class={subcls(outlineC, 'notch')}>
+        <label class={floatingLabelC}>{label}</label>
+      </div>
+      <div class={subcls(outlineC, 'trailing')}></div>
+    </div>
+  {/if}
+  {#if !fullwidth && !outlined && label}
+    <label class={floatingLabelC}>{label}</label>
+  {/if}
+  {#if !outlined}
     <div class="mdc-line-ripple"></div>
-  <!-- {/if} -->
- </div>
+  {/if}
+</div>
