d6c00acca0a7bc68c27557ba10fb3289a66c17b6
Hosein Khansari <hrk.x11@gmail.com>
2019-08-23 23:40:54 +0430

    (ListItem) add tabindex property
    Note: it is required to use a tabindex on the first list item in a drawer. Otherwise MDCDrawer will raise an error.

--- src/List/ListItem.svelte
+++ src/List/ListItem.svelte
@@ -1,8 +1,9 @@
 <script>
   import { ripple } from '../actions';
+  export let tabindex = void 0;
 </script>
 
-<li class="mdc-list-item" use:ripple>
+<li class="mdc-list-item" use:ripple {tabindex}>
   <div class="mdc-list-item__text">
     <slot></slot>
   </div>
