dc67a811e778ece6176c7781cbe8c98c290c65a2
Hosein Khansari <hrk.x11@gmail.com>
2019-08-23 22:24:45 +0430

    (Fab) add href prop, convert unknown props to attributes

--- src/Fab.svelte
+++ src/Fab.svelte
@@ -1,26 +1,44 @@
 <script>
 import { ripple } from './actions';
-import { createClassname } from './helpers';
+import { createClassname, omit } from './helpers';
 import Icon from './Icon.svelte';
 export let icon = '',
   extended = false,
   exited = false,
   fixed = false,
   iconTrailing = false,
+  href = '',
   mini = false;
+$: props = omit($$props, 'icon', 'extended', 'fixed', 'iconTrailing', 'href', 'mini', 'class');
 $: className = createClassname('fab', { extended, mini, exited, fixed })
 </script>
 
-<button use:ripple class={className} on:click>
-  {#if icon && !iconTrailing}
-    <div class="mdc-fab__icon"><Icon>{icon}</Icon></div>
-  {/if}
-  {#if extended}
-    <span class="mdc-fab__label">
-      <slot></slot>
-    </span>
-  {/if}
-  {#if icon && iconTrailing}
-    <div class="mdc-fab__icon"><Icon>{icon}</Icon></div>
-  {/if}
-</button>
+{#if href === undefined}
+  <button {...props} use:ripple class={className} on:click>
+    {#if icon && !iconTrailing}
+      <div class="mdc-fab__icon"><Icon>{icon}</Icon></div>
+    {/if}
+    {#if extended}
+      <span class="mdc-fab__label">
+        <slot></slot>
+      </span>
+    {/if}
+    {#if icon && iconTrailing}
+      <div class="mdc-fab__icon"><Icon>{icon}</Icon></div>
+    {/if}
+  </button>
+{:else}
+  <a {...props} {href} use:ripple class={className} on:click>
+    {#if icon && !iconTrailing}
+      <div class="mdc-fab__icon"><Icon>{icon}</Icon></div>
+    {/if}
+    {#if extended}
+      <span class="mdc-fab__label">
+        <slot></slot>
+      </span>
+    {/if}
+    {#if icon && iconTrailing}
+      <div class="mdc-fab__icon"><Icon>{icon}</Icon></div>
+    {/if}
+  </a>
+{/if}
