f88876d68fe0bbd03ef342076cc4570e832a92ea
Hosein Khansari <hrk.x11@gmail.com>
2019-08-31 08:56:18 +0430

    remove TabIndicator & TabScroller from exports

--- src/Tabs/index.js
+++ src/Tabs/index.js
@@ -1,4 +1,4 @@
-export { default as TabScroller } from './TabScroller.svelte';
-export { default as TabIndicator } from './TabIndicator.svelte';
+// export { default as TabScroller } from './TabScroller.svelte';
+// export { default as TabIndicator } from './TabIndicator.svelte';
 export { default as TabBar } from './TabBar.svelte';
 export { default as Tab } from './Tab.svelte';
