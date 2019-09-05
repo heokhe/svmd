b19419ea3cd770dba39f2f32f0c7b3018af778d9
Hosein Khansari <hrk.x11@gmail.com>
2019-09-05 20:55:01 +0430

    Add href prop to ListItem

--- src/List/ListItem.svelte
+++ src/List/ListItem.svelte
@@ -1,10 +1,19 @@
 <script>
   import { ripple } from '../actions';
-  export let tabindex = void 0;
+  export let tabindex = void 0,
+    href = undefined;
 </script>
 
-<li class="mdc-list-item" use:ripple {tabindex}>
-  <div class="mdc-list-item__text">
-    <slot></slot>
-  </div>
-</li>
+{#if href !== undefined}
+  <a {href} class="mdc-list-item" use:ripple {tabindex}>
+    <div class="mdc-list-item__text">
+      <slot></slot>
+    </div>
+  </a>
+{:else}
+  <li class="mdc-list-item" use:ripple {tabindex}>
+    <div class="mdc-list-item__text">
+      <slot></slot>
+    </div>
+  </li>
+{/if}
