b8dc6d3eed1d59fc5fe8826eefc5b2097dd48160
Hosein Khansari <hrk.x11@gmail.com>
2019-09-07 09:52:32 +0430

    Add icon graphics support to ListItem

--- src/List/ListItem.svelte
+++ src/List/ListItem.svelte
@@ -1,17 +1,26 @@
 <script>
   import { ripple } from '../actions';
+  import { omit } from '../helpers';
   export let tabindex = void 0,
-    href = undefined;
+    href = undefined,
+    icon = '';
+  $: props = omit($$props, 'href', 'class', icon);
 </script>
 
 {#if href !== undefined}
-  <a {href} class="mdc-list-item" use:ripple {tabindex}>
+  <a {...props} {href} class="mdc-list-item" use:ripple>
+    {#if icon}
+      <i class="material-icons mdc-list-item__graphic" aria-hidden="true">{icon}</i>
+    {/if}
     <div class="mdc-list-item__text">
       <slot></slot>
     </div>
   </a>
 {:else}
-  <li class="mdc-list-item" use:ripple {tabindex}>
+  <li {...props} class="mdc-list-item" use:ripple>
+    {#if icon}
+      <i class="material-icons mdc-list-item__graphic" aria-hidden="true">{icon}</i>
+    {/if}
     <div class="mdc-list-item__text">
       <slot></slot>
     </div>
