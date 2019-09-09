60b8f2cec1fc3b62620c90c0ae60926b1566ebb3
Hosein Khansari <hrk.x11@gmail.com>
2019-09-10 00:23:34 +0430

    Add Radio component

---
+++ src/Radio.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { getContext } from 'svelte';
+  import { cls, subcls, omit } from './helpers';
+  import { wrap } from './actions';
+  import { MDCRadio } from '@material/radio';
+
+  export let disabled = false,
+    group,
+    value;
+
+  let self, event;
+  const formField = getContext('SVMD:form-field'),
+    mdc = wrap(MDCRadio, {
+      initialize(radio) {
+        self = radio;
+        event = radio.root_.addEventListener('input', ( )=> {
+          if (radio.checked) group = value;
+        });
+      },
+      destroy({ root_: el }) {
+        el.removeEventListener(event)
+      }
+    });
+
+  $: if (formField && self) {
+    formField.update(ff => {
+      ff.input = self;
+    });
+  }
+  $: checked = group === value;
+  $: c = cls('radio', { disabled });
+  $: props = omit($$props, 'value', 'group', 'disabled', 'class');
+</script>
+
+<div class="{c} {$$props.class}" use:mdc>
+  <input
+    {...props}
+    type="radio" {checked} {value} {disabled}
+    class={subcls(c, 'native-control')}>
+  <div class={subcls(c, 'background')}>
+    <div class={subcls(c, 'inner-circle')}></div>
+    <div class={subcls(c, 'outer-circle')}></div>
+  </div>
+</div>
--- src/components.js
+++ src/components.js
@@ -8,6 +8,7 @@ export { default as Grid, default as LayoutGrid } from './Grid.svelte';
 export { default as Icon } from './Icon.svelte';
 export { default as IconButton } from './IconButton.svelte';
 export * from './List';
+export { default as Radio } from './Radio.svelte';
 export { default as Slider } from './Slider.svelte';
 export { default as Snackbar } from './Snackbar.svelte';
 export { default as Switch } from './Switch.svelte';
