a3bdb5ea8bf8df7c26f6594a4fea1e95bc7daa58
Hosein Khansari <hrk.x11@gmail.com>
2019-09-14 17:22:17 +0430

    export new list components 🤦‍♂️

--- src/List/index.js
+++ src/List/index.js
@@ -2,5 +2,8 @@ export { default as List } from './List.svelte';
 export { default as ListDivider } from './ListDivider.svelte';
 export { default as ListGroup } from './ListGroup.svelte';
 export { default as ListItem } from './ListItem.svelte';
+export { default as ListItemContent } from './ListItemContent.svelte';
+export { default as ListItemMeta } from './ListItemMeta.svelte';
+export { default as ListItemGraphic } from './ListItemGraphic.svelte';
 export { default as ListSubheader } from './ListSubheader.svelte';
 export { default as ListText } from './ListText.svelte';
