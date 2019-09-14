72f1995938b0047d717c99d78a4c1426922641db
Hosein Khansari <hrk.x11@gmail.com>
2019-09-14 20:06:38 +0430

    (example page) Extract the drawer to a separate file

--- examples/App.svelte
+++ examples/App.svelte
@@ -1,129 +1,102 @@
 <script>
-import {
-  Button, TopAppBar, Drawer, Fab, TabBar, Tab, Slider, Radio,
-  Input, IconButton, Icon, Switch, List, ListItem, FormField,
-  ListText, ListDivider, Grid, Cell, H6, Snackbar, Checkbox
-} from '../src/components';
+  import AppDrawer from './AppDrawer.svelte';
+  import {
+    Button, TopAppBar, Fab, TabBar, Tab, Slider,
+    Input, IconButton, Icon, Switch, FormField,
+    Grid, Cell, H6, Snackbar, Checkbox, Radio
+  } from '../src/components';
 
-let drawerOpen = false,
-  activeTab = 0,
-  slider = 35,
-  title = 'SVMD Playground',
-  checked = false,
-  snackbar = false,
-  color = '';
+  let drawerOpen = false,
+    activeTab = 0,
+    slider = 35,
+    checked = false,
+    snackbar = false,
+    color = 'blue';
 
-const toggleDrawer = () => drawerOpen = !drawerOpen;
-const reload = () => window.location.reload();
+  const toggleDrawer = () => drawerOpen = !drawerOpen;
+  const reload = () => window.location.reload();
 </script>
 
-<svelte:head>
-  <title>{title}</title>
-</svelte:head>
-
-<Drawer bind:open={drawerOpen} modal withHeader>
-  <span slot="title">Drawer</span>
-  <span slot="subtitle">Select an item</span>
-  <List>
-    <ListItem tabindex="0">
-      Installation
-    </ListItem>
-    <ListItem icon="book">
-      Documentation
-    </ListItem>
-  </List>
-  <ListDivider />
-  <List twoLine>
-    <ListItem icon="web">
-      <ListText>Website</ListText>
-      <ListText secondary>See it!</ListText>
-    </ListItem>
-  </List>
-</Drawer>
-
-<TopAppBar {title} on:nav-icon-click={toggleDrawer} fixed>
-  <IconButton on:click={reload}>
-    <Icon>refresh</Icon>
-  </IconButton>
+<AppDrawer bind:open={drawerOpen} />
+<TopAppBar title="SVMD Playground" on:nav-icon-click={toggleDrawer} fixed>
+  <IconButton on:click={reload}><Icon>refresh</Icon></IconButton>
 </TopAppBar>
-
 <div class="mdc-top-app-bar--fixed-adjust">
-  <div class="mdc-drawer-app-content">
-    <Grid>
-      <Cell span="6">
-        <H6>Buttons</H6>
-        <Button>regular button</Button>
-        <Button outlined>outlined</Button>
-        <Button raised icon="favorite">raised with icon</Button>
-        <Button unelevated>unelevated</Button>
-      </Cell>
+  <Grid>
+    <Cell span="6">
+      <H6>Buttons</H6>
+      <Button>regular button</Button>
+      <Button outlined>outlined</Button>
+      <Button raised icon="favorite">raised with icon</Button>
+      <Button unelevated>unelevated</Button>
+    </Cell>
 
-      <Cell span="6">
-        <H6>Tabs</H6>
-        <TabBar bind:active={activeTab}>
-          <Tab icon="settings">settings</Tab>
-          <Tab icon="photo">gallery</Tab>
-          <Tab icon="person">contacts</Tab>
-        </TabBar>
-      </Cell>
+    <Cell span="6">
+      <H6>Tabs</H6>
+      <TabBar bind:active={activeTab}>
+        <Tab icon="settings">settings</Tab>
+        <Tab icon="photo">gallery</Tab>
+        <Tab icon="person">contacts</Tab>
+      </TabBar>
+    </Cell>
 
-      <Cell span="6">
-        <H6>Sliders</H6>
-        <Slider bind:value={slider} step={1} />
-        <Slider bind:value={slider} step={1} discrete />
-        <Slider bind:value={slider} step={10} discrete displayMarkers />
-        Value: {slider}/100
-      </Cell>
+    <Cell span="6">
+      <H6>Sliders</H6>
+      <Slider bind:value={slider} step={1} />
+      <Slider bind:value={slider} step={1} discrete />
+      <Slider bind:value={slider} step={10} discrete displayMarkers />
+      Value: {slider}/100
+    </Cell>
 
-      <Cell span="6">
-        <H6>Text fields</H6>
-        <Grid noPadding>
-          <Cell desktop="6" phone="4">
-            <Input trailingIcon="close" outlined bind:value={title} placeholder="Playground" label="Page Title" />
-          </Cell>
-          <Cell desktop="3" phone="2">
-            <Input outlined type="number" inputmode="numeric" min="0" max="100" step="1" bind:value={slider} label="Slider value" />   
-          </Cell>
-          <Cell desktop="3" phone="2">
-            <Input type="number" inputmode="numeric" min="0" max="100" step="1" bind:value={slider} label="Slider value" />  
-          </Cell>
-        </Grid>
-      </Cell>
+    <Cell span="6">
+      <H6>Text fields</H6>
+      <Grid noPadding>
+        <Cell desktop="6" phone="4">
+          <Input trailingIcon="close" outlined placeholder="John Doe" label="Your Name" />
+        </Cell>
+        <Cell desktop="3" phone="2">
+          <Input outlined type="number" inputmode="numeric" min="0" max="100" step="1" bind:value={slider} label="Slider value" />
+        </Cell>
+        <Cell desktop="3" phone="2">
+          <Input type="number" inputmode="numeric" min="0" max="100" step="1" bind:value={slider} label="Slider value" />  
+        </Cell>
+      </Grid>
+    </Cell>
 
-      <Cell span="2">
-        <H6>Snackbars</H6>
-        <Button on:click={() => snackbar = true} raised>open it!</Button>
-        <Snackbar bind:active={snackbar}>
-          This is an important message.
-        </Snackbar>
-      </Cell>
+    <Cell span="2">
+      <H6>Snackbars</H6>
+      <Button on:click={() => snackbar = true} raised>open it!</Button>
+      <Snackbar bind:active={snackbar}>
+        This is an important message.
+      </Snackbar>
+    </Cell>
 
-      <Cell span="2">
-        <H6>Radio Buttons</H6>
-        {#each ['red', 'green', 'blue'] as value}
-          <FormField>
-            <Radio id="radio-{value}" {value} bind:group={color} />
-            <label for="radio-{value}" style="text-transform: capitalize">{value}</label>
-          </FormField>
-          <br>
-        {/each}
-        <span>Selected color: {color}</span>
-      </Cell>
-
-      <Cell span="2">
-        <H6>Switches</H6>
-        <Switch bind:checked />
-      </Cell>
-
-      <Cell span="2">
-        <H6>Checkboxes</H6>
+    <Cell span="2">
+      <H6 style="color: {color}">Radio Buttons</H6>
+      {#each ['red', 'green', 'blue'] as value}
         <FormField>
-          <Checkbox id="mycheckbox" bind:checked />
-          <label for="mycheckbox">{checked ? 'On' : 'Off'} ({checked})</label>
+          <Radio id="radio-{value}" {value} bind:group={color} />
+          <label for="radio-{value}" style="text-transform: capitalize">{value}</label>
         </FormField>
-      </Cell>
-    </Grid>
-    
-    <Fab icon="code" fixed extended href="https://github.com/hkh12/svmd" target="_blank">open on github</Fab>
-  </div>
+        <br>
+      {/each}
+    </Cell>
+
+    <Cell span="2">
+      <H6>Switches</H6>
+      <Switch bind:checked />
+    </Cell>
+
+    <Cell span="2">
+      <H6>Checkboxes</H6>
+      <FormField>
+        <Checkbox id="mycheckbox" bind:checked />
+        <label for="mycheckbox">{checked ? 'On' : 'Off'} ({checked})</label>
+      </FormField>
+    </Cell>
+  </Grid>
+  
+  <Fab icon="code" fixed extended href="https://github.com/hkh12/svmd" target="_blank">
+    open on github
+  </Fab>
 </div>
---
+++ examples/AppDrawer.svelte
@@ -0,0 +1,40 @@
+<script>
+  import {
+    Drawer, List, ListGroup, ListDivider, ListItem, Icon, ListText,
+    ListItemContent, ListItemMeta, ListSubheader, ListItemGraphic
+  } from '../src/components';
+  export let open;
+</script>
+
+<Drawer bind:open modal>
+  <ListGroup>
+    <ListSubheader>One line</ListSubheader>
+    <List>
+      <ListItem tabindex="0">
+        <ListItemGraphic><Icon>person</Icon></ListItemGraphic>
+        <ListItemContent>Hosein</ListItemContent>
+        <ListItemMeta>Admin</ListItemMeta>
+      </ListItem>
+      <ListItem>
+        <ListItemGraphic><Icon>dns</Icon></ListItemGraphic>
+        <ListItemContent>Madeline</ListItemContent>
+        <ListItemMeta>Gohkhor</ListItemMeta>
+      </ListItem>
+    </List>
+  </ListGroup>
+  <ListDivider />
+  <ListGroup>
+    <ListSubheader>Two lines</ListSubheader>
+    <List twoLine>
+      <ListItem href="https://github.com/hkh12/svmd" target="_blank">
+        <ListItemGraphic>
+          <Icon>code</Icon>
+        </ListItemGraphic>
+        <ListItemContent>
+          <ListText>Source code</ListText>
+          <ListText secondary>Available on GitHub</ListText>
+        </ListItemContent>
+      </ListItem>
+    </List>
+  </ListGroup>
+</Drawer>
