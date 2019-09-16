d5cd8bb53c7a5db442a10bd3ab677c93b13f38b1
Hosein Khansari <hrk.x11@gmail.com>
2019-09-16 17:00:13 +0430

    Add checkbox & radio support to List

--- src/List/List.svelte
+++ src/List/List.svelte
@@ -1,21 +1,28 @@
 <script>
+  import { setContext } from 'svelte';
+  import { writable } from 'svelte/store';
   import { MDCList } from '@material/list';
   import { wrap } from '../actions';
   import { cls, omit } from '../helpers';
 
   export let dense = false,
-    twoLine = false;
+    twoLine = false,
+    checklist = false,
+    radio = false;
 
   const mdc = wrap(MDCList, {
     initialize(list) {
       list.wrapFocus = true;
     }
-  })
+  });
+  const role = writable('');
+  setContext('svmd:list:role', role);
 
+  $: $role = checklist ? 'group' : radio ? 'radiogroup' : 'list';
   $: className = cls('list', { dense, twoLine });
-  $: props = omit($$props, 'dense', 'twoLine', 'class');
+  $: props = omit($$props, 'dense', 'twoLine', 'class', 'checklist', 'radio');
 </script>
 
-<ul {...props} class={className} use:mdc>
+<ul {...props} class={className} role={$role} use:mdc>
   <slot></slot>
 </ul>
--- src/List/ListItem.svelte
+++ src/List/ListItem.svelte
@@ -1,18 +1,26 @@
 <script>
+  import { getContext, onMount } from 'svelte';
   import { ripple } from '../actions';
   import { omit } from '../helpers';
 
   export let href = undefined;
 
+  const listRole = getContext('svmd:list:role');
+  $: role = ({
+    group: 'checkbox',
+    radiogroup: 'radio'
+  })[$listRole];
+
   $: props = omit($$props, 'href', 'class');
+  $: c = `mdc-list-item mdc-ripple-upgraded ${$$props.class}`;
 </script>
 
 {#if href !== undefined}
-  <a {...props} {href} class="mdc-list-item {$$props.class}" use:ripple>
+  <a {...props} {role} {href} class={c} use:ripple>
     <slot></slot>
   </a>
 {:else}
-  <li {...props} class="mdc-list-item {$$props.class}" use:ripple>
+  <li {...props} {role} class={c} use:ripple>
     <slot></slot>
   </li>
 {/if}
