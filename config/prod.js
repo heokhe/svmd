63825fdf69376314f8a8e3295135996668e797ce
Hosein Khansari <hrk.x11@gmail.com>
2019-09-18 14:52:54 +0430

    [chore] Change directory structure
    - rename components.js to index.js
    - import styles in a hidden file named .styles.js

--- config/prod.js
+++ config/prod.js
@@ -1,22 +1,76 @@
 const { resolve } = require('path'),
   MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const createConfig = ({ esModule, includeStyles }) => ({
+const createJSconfig = (esModule = false) => ({
   mode: 'production',
-  entry: `./src/${includeStyles ? 'index' : 'components'}.js`,
+  entry: './src/index.js',
   output: {
     path: resolve(__dirname, '../dist'),
-    filename: `svmd.${esModule ? 'es.js' : 'js'}`,
+    filename: `svmd${esModule ? '.es' : ''}.js`,
     ...!esModule && { libraryTarget: 'commonjs' }
   },
   module: {
-    rules: [
-      {
-        test: /\.(html|svelte)$/,
-        exclude: /node_modules/,
-        use: 'svelte-loader'
-      },
-      ...includeStyles ? [{
+    rules: [{
+      test: /\.(html|svelte)$/,
+      exclude: /node_modules/,
+      use: 'svelte-loader'
+    }]
+  }
+});
+
+// const createConfig = ({ esModule, isStyle = false }) => ({
+//   mode: 'production',
+//   entry: `./src/${isStyle ? '.styles' : 'index'}.js`,
+//   output: {
+//     path: resolve(__dirname, '../dist'),
+//     filename: `svmd.${esModule ? 'es.js' : 'js'}`,
+//     ...!esModule && { libraryTarget: 'commonjs' }
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(html|svelte)$/,
+//         exclude: /node_modules/,
+//         use: 'svelte-loader'
+//       },
+//       ...includeStyles ? [{
+//         test: /\.s[ac]ss$/,
+//         use: [
+//           MiniCssExtractPlugin.loader,
+//           'css-loader',
+//           {
+//             loader: 'sass-loader',
+//             options: {
+//               sassOptions: {
+//                 includePaths: [resolve('node_modules')]
+//               }
+//             }
+//           }
+//         ]
+//       }] : []
+//     ]
+//   },
+//   ...includeStyles && {
+//     plugins: [
+//       new MiniCssExtractPlugin({
+//         filename: 'svmd.css'
+//       })
+//     ]
+//   }
+// });
+
+module.exports = [
+  createJSconfig(true),
+  createJSconfig(false),
+  {
+    mode: 'production',
+    entry: './src/.styles.js',
+    output: {
+      path: resolve(__dirname, '../dist'),
+      filename: '.styles.js'
+    },
+    module: {
+      rules: [{
         test: /\.s[ac]ss$/,
         use: [
           MiniCssExtractPlugin.loader,
@@ -30,16 +84,12 @@ const createConfig = ({ esModule, includeStyles }) => ({
             }
           }
         ]
-      }] : []
-    ]
-  },
-  ...includeStyles && {
+      }]
+    },
     plugins: [
       new MiniCssExtractPlugin({
         filename: 'svmd.css'
       })
     ]
   }
-});
-
-module.exports = [createConfig({ esModule: true }), createConfig({ includeStyles: true })];
+];
