e7a92f06ad2dc5c026d7c670417d5d66fe6aefd1
Hosein Khansari <hrk.x11@gmail.com>
2019-09-28 22:15:16 +0330

    Make Slider work

--- src/Slider.svelte
+++ src/Slider.svelte
@@ -1,7 +1,7 @@
 <script>
-  import { onMount } from 'svelte';
+  import { onMount, tick } from 'svelte';
   import { wrap } from './actions';
-  import { createClassname } from './helpers';
+  import { cls, clamp } from './helpers';
 
   export let discrete = false,
     disabled = false,
@@ -11,31 +11,52 @@
     displayMarkers = false,
     step = 0;
 
-  let root;
-  onMount(() => {
-    let width = root.clientWidth;
-    console.log('Debug: width', width, value, value / 100 * width)
-  })
+  /** @type {Element} */
+  let root, track, thumb;
+  let width = 0;
+  let mousedown = false;
 
-  $: className = createClassname('slider', { discrete, displayMarkers });
+  function setValueFromMouseEvent(event) {
+    const { clientX: x } = event.touches ? event.touches[0] : event;
+    const px = x - root.getBoundingClientRect().x;
+    const pr = px / width
+    value = clamp((pr * (max - min)) + min, { step, min, max })
+  }
+  function handleMouseDown(event) {
+    mousedown = true;
+    setValueFromMouseEvent(event);
+  }
+  function handleMouseMove(event) {
+    if (mousedown) setValueFromMouseEvent(event);
+  }
+  const handleMouseUp = () => mousedown && (mousedown = false);
+  $: percentage = (value - min) / (max - min) * 100;
+  $: if (thumb) thumb.style.transform = `translateX(${percentage / 100 * width}px) translateX(-50%)`;
+  $: if (track) track.style.transform = `scaleX(${percentage / 100})`
+  $: className = cls('slider', {
+    discrete, displayMarkers,
+    active: mousedown, /* inTransit: mousedown */
+  });
 </script>
 
-<div bind:this={root}
+<svelte:window on:mousemove={handleMouseMove} on:mouseup={handleMouseUp} on:touchmove={handleMouseDown} on:touchend={handleMouseUp}></svelte:window>
+<div bind:this={root} bind:clientWidth={width}
+  on:mousedown={handleMouseDown}
   class={className}
   tabindex="0" role="slider"
   aria-valuemin={min} aria-valuemax={max}
   aria-valuenow={value} data-step={step}
   aria-disabled={disabled}>
     <div class="mdc-slider__track-container">
-      <div class="mdc-slider__track"></div>
+      <div class="mdc-slider__track" bind:this={track}></div>
       {#if discrete && displayMarkers}
         <div class="mdc-slider__track-marker-container"></div>
       {/if}
     </div>
-    <div class="mdc-slider__thumb-container">
+    <div class="mdc-slider__thumb-container" bind:this={thumb}>
       {#if discrete}
         <div class="mdc-slider__pin">
-          <span class="mdc-slider__pin-value-marker"></span>
+          <span class="mdc-slider__pin-value-marker">{value}</span>
         </div>
       {/if}
       <svg class="mdc-slider__thumb" width="21" height="21">
