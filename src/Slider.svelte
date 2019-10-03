234f4fb2fa49115e22e153677f11a5d50b2161ca
Hosein Khansari <hrk.x11@gmail.com>
2019-10-03 13:16:58 +0330

    (Slider) Add keyboard support, disable transitions (for now)

--- src/Slider.svelte
+++ src/Slider.svelte
@@ -15,7 +15,8 @@
   let root, track, thumb;
   let width = 0;
   let active = false;
-  let inTransit = false;
+  let focus = false;
+  // let inTransit = true;
 
   function setValueFromMouseEvent({ clientX, touches: [touch] = [] }) {
     const x = touch ? touch.clientX : clientX;
@@ -26,19 +27,38 @@
   function handleMouseDown(event) {
     const { path, button } = event;
     if (button === 2 || !path.includes(root)) return;
-    inTransit = !path.includes(thumb);
-    setTimeout(() => (inTransit = false), 200);
+    // inTransit = !path.includes(thumb);
+    // setTimeout(() => (inTransit = false), 200);
     active = true;
     setValueFromMouseEvent(event);
   }
+  function handleKeydown(event) {
+    const { key } = event,
+      st = step || 1,
+      diff = key === 'ArrowLeft' ? -st :
+        key === 'ArrowRight' ? st :
+        key === 'ArrowUp' ? st :
+        key === 'ArrowDown' ? -st :
+        key === 'PageUp' ? st * 4 :
+        key === 'PageDown' ? st * -4 :
+        key === 'Home' ? -value :
+        key === 'End' ? max - value : undefined;
+    if (diff !== undefined) {
+      event.preventDefault();
+      value = clamp(value + diff, { min, max, step })
+    }
+  }
   const handleMouseMove = evt => !evt.touches && active && setValueFromMouseEvent(evt);
+  // const handleMouseUp = () => (inTransit = true) && active && (active = false);
   const handleMouseUp = () => active && (active = false);
 
+  $: if (discrete && displayMarkers && !step) step = 1;
   $: clampedValue = clamp(value, { min, max, step });
   $: percentage = (clampedValue - min) / (max - min) * 100;
   $: if (thumb) thumb.style.transform = `translateX(${percentage / 100 * width}px) translateX(-50%)`;
   $: if (track) track.style.transform = `scaleX(${percentage / 100})`;
-  $: className = cls('slider', { discrete, displayMarkers, active, inTransit });
+  // $: className = cls('slider', { discrete, displayMarkers, active, inTransit });
+  $: className = cls('slider', { discrete, displayMarkers, active, focus });
 </script>
 
 <svelte:window
@@ -46,9 +66,11 @@
   on:mouseup={handleMouseUp}
   on:touchmove={handleMouseDown}
   on:touchend={handleMouseUp}></svelte:window>
+
 <div bind:this={root} bind:clientWidth={width}
-  on:mousedown={handleMouseDown} class={className}
-  tabindex="0" role="slider"
+  on:focus={() => focus = !active} on:blur={() => focus = false}
+  on:mousedown={handleMouseDown} on:keydown={handleKeydown}
+  class={className} tabindex="0" role="slider"
   aria-valuemin={min} aria-valuemax={max}
   aria-valuenow={clampedValue} aria-disabled={disabled}>
     <div class="mdc-slider__track-container">
