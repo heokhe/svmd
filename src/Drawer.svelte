fcb3d237bdf153ed08c18e3b93a17b591448a65e
Hosein Khansari <hrk.x11@gmail.com>
2019-10-06 19:38:03 +0330

    (Drawer) Add JS logic

--- src/Drawer.svelte
+++ src/Drawer.svelte
@@ -1,44 +1,72 @@
 <script>
 import { wrap } from './actions';
-import { createClassname } from './helpers';
-// import { MDCDrawer } from '@material/drawer';
+import { cls, subcls, nextFrame } from './helpers';
 export let dismissible = false,
   modal = false,
   open = false,
   mode = 'modal',
   withHeader = false;
-// const mdc = wrap(MDCDrawer, {
-//   initialize(drawer) {
-//     // fixes scrim_ === null
-//     setTimeout(() => {
-//       if (!drawer.focusTrap_) drawer.focusTrap_ = drawer.focusTrapFactory_();
-//       if (!drawer.scrim_) drawer.initialSyncWithDOM();
-//       drawer.wrapFocus = true
-//       drawer.listen('MDCDrawer:closed', () => open = false);
-//     }, 0);
-//   },
-//   initAndUpdate(drawer, open) {
-//     if (!!open !== drawer.open) drawer.open = !!open
-//   }
-// })
-$: className = createClassname('drawer', { modal, dismissible })
+
+const onUpdate = () => {
+  if (open) openTheDrawer();
+  return {
+    update: ({ open: isOpen }) => console.log({ open, isOpen, active }) || (!active ? openTheDrawer : closeTheDrawer)()
+  }
+}
+
+let active = false;
+let opening = false;
+let animate = false;
+let closing = false;
+let drawer;
+$: type = modal ? 'modal' : 'dismissible';
+function transitionEnd() {
+  return new Promise(resolve => {
+    drawer.addEventListener('transitionend', async () => {
+      await nextFrame();
+      resolve();
+    }, { once: true })
+  })
+}
+async function openTheDrawer() {
+  active = true;
+  await nextFrame();
+  animate = true;
+  await nextFrame();
+  opening = true;
+  await nextFrame();
+  animate = false;
+  await transitionEnd();
+  opening = false;
+}
+async function closeTheDrawer() {
+	console.log('Debug: closing 3', closing)
+  closing = true;
+  opening = false;
+  animate = false;
+  await transitionEnd();
+  closing = false;
+  active = false;
+}
+
+$: c = cls('drawer', { [type]: true, open: active, opening, animate, closing })
 </script>
 
-<aside  class={className}>
+<aside class={c} bind:this={drawer} use:onUpdate={{ open }}>
   {#if withHeader}
-    <div class="mdc-drawer__header">
-      <div class="mdc-drawer__title">
+    <div class={subcls(c, 'header')}>
+      <div class={subcls(c, 'title')}>
         <slot name="title"></slot>
       </div>
-      <div class="mdc-drawer__subtitle">
+      <div class={subcls(c, 'subtitle')}>
         <slot name="subtitle"></slot>
       </div>
     </div>
   {/if}
-  <div class="mdc-drawer__content">
+  <div class={subcls(c, 'content')}>
     <slot></slot>
   </div>
 </aside>
 {#if modal}
-  <div class="mdc-drawer-scrim"></div>
+  <div on:click={closeTheDrawer} class="mdc-drawer-scrim"></div>
 {/if}
